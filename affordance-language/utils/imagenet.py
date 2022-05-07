6cc4b2da346376dde1eb760e24ce3f939a54d007
rex131 <87143368+rex131@users.noreply.github.com>
2022-05-07 20:14:35 +0800

    Add files via upload

---
+++ affordance-language/utils/imagenet.py
@@ -0,0 +1,33 @@
+import json
+import os
+import csv
+
+# Generates a dictionary mapping ImageNet images to their labels
+
+file_name = '/home/turtlepc-04/affordance-language/data/map_clsloc.txt' #contains mapping between ImageNet classes and labels
+#subtitute with your own path to the ILSVRC2012 validation images and image labels
+labels = '/home/turtlepc-04/Downloads/ILSVRC2012_devkit_t12/data/ILSVRC2012_validation_ground_truth.txt'
+folder = '/home/turtlepc-04/Downloads/ILSVRC2012_img_val'
+
+objects = {}
+with open(file_name) as filename:
+    for line in filename:
+        label = str(line.split()[1])
+        obj = str(line.split()[2]).split('_')
+        object = ''
+        for o in obj:
+            object += o + ' '
+        objects[label] = object[:-1]
+
+images = os.listdir(folder)
+images.sort()
+image_label = {}
+with open(labels) as f:
+    for i, line in enumerate(f):
+        image = images[i]
+        label = line[:-1]
+        image_label[image] = objects[label]
+
+with open('/home/turtlepc-04/affordance-language/data/image_label.json', 'w+') as f:
+    f.write(json.dumps(image_label))
+    print('done!')
